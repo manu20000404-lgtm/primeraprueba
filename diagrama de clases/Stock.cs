712c15ce474f68fa4ea36475dc2ed62e35f6b96f
Mamu <manu20000404@gmail.com>
2026-09-21 09:43:32 -0300

    manuel
    hola

---
+++ diagrama de clases/Stock.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diagrama_de_clases
+{
+    internal class Stock
+    {
+    }
+}
