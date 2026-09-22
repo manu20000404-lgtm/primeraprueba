e03cb18fbfd0b9491ef5e866ab530aa3e4a78276
felix-alelen <aguayoarias608@gmail.com>
2026-09-22 09:04:57 -0300

    Update Producto.cs

--- diagrama de clases/Producto.cs
+++ diagrama de clases/Producto.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace diagrama_de_clases
+namespace TiendaPOO
 {
-    internal class Producto
+    public class Producto
     {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+        public string Categoria { get; set; }
+
+        // Constructor principal
+        public Producto(string codigo, string nombre, double precio, string categoria)
+        {
+            if (precio < 0)
+            {
+                throw new ArgumentException("El precio no puede ser negativo.");
+            }
+
+            Codigo = codigo;
+            Nombre = nombre;
+            Precio = precio;
+            Categoria = categoria;
+        }
+
+        // Constructor sobrecargado
+        public Producto(string codigo, string nombre, double precio)
+            : this(codigo, nombre, precio, "General")
+        {
+        }
+
+        public void MostrarInfo()
+        {
+            Console.WriteLine(
+                $"{Codigo} - {Nombre} | Categoría: {Categoria} | Precio: ${Precio:N0}");
+        }
     }
 }
