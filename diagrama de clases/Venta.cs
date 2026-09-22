384d693dbbed36fa7ed4da3ae7c77cec476e9d9e
Junemc <manu20000404@gmail.com>
2026-09-22 09:46:13 -0300

    Update Venta.cs

--- diagrama de clases/Venta.cs
+++ diagrama de clases/Venta.cs
@@ -8,5 +8,16 @@ namespace diagrama_de_clases
 {
     internal class Venta
     {
+
+public class Venta
+{
+    public string Numero { get; set; }
+    public DateTime Fecha { get; set; }
+
+    public double CalcularTotal()
+    {
+        throw new NotImplementedException();
+    }
+}
     }
 }
