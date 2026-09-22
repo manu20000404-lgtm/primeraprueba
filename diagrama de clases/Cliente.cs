98e81b214fe884a0efc40426c3e1e0bc255e7f01
felix-alelen <aguayoarias608@gmail.com>
2026-09-22 09:01:24 -0300

    Update Cliente.cs

--- diagrama de clases/Cliente.cs
+++ diagrama de clases/Cliente.cs
@@ -1,12 +1,43 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace diagrama_de_clases
+namespace TiendaPOO
 {
-    internal class Cliente
+    public class Cliente
     {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+
+        private List<Venta> historialVentas;
+
+        public Cliente(string id, string nombre, string email)
+        {
+            Id = id;
+            Nombre = nombre;
+            Email = email;
+            historialVentas = new List<Venta>();
+        }
+
+        public void AgregarVenta(Venta venta)
+        {
+            historialVentas.Add(venta);
+        }
+
+        public List<Venta> HistorialCompras()
+        {
+            return historialVentas;
+        }
+
+        public void MostrarHistorial()
+        {
+            Console.WriteLine($"\n--- Historial de {Nombre} ---");
+
+            foreach (Venta venta in historialVentas)
+            {
+                Console.WriteLine(
+                    $"Venta {venta.Numero}: ${venta.CalcularTotal():N0}");
+            }
+        }
     }
 }
