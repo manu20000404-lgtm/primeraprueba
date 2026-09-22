884bb9214ce3d40c5d21200458da78401f1bdd60
felix-alelen <aguayoarias608@gmail.com>
2026-09-22 09:07:38 -0300

    Update Stock.cs

--- diagrama de clases/Stock.cs
+++ diagrama de clases/Stock.cs
@@ -1,12 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace diagrama_de_clases
+namespace TiendaPOO
 {
-    internal class Stock
+    public class Stock
     {
+        private int cantidadDisponible;
+        private int stockMinimo;
+
+        public Producto Producto { get; private set; }
+
+        public int CantidadDisponible
+        {
+            get { return cantidadDisponible; }
+        }
+
+        public int StockMinimo
+        {
+            get { return stockMinimo; }
+        }
+
+        public Stock(Producto producto, int cantidadInicial, int stockMinimo)
+        {
+            if (cantidadInicial < 0)
+            {
+                throw new ArgumentException(
+                    "La cantidad inicial no puede ser negativa.");
+            }
+
+            if (stockMinimo < 0)
+            {
+                throw new ArgumentException(
+                    "El stock mínimo no puede ser negativo.");
+            }
+
+            Producto = producto;
+            cantidadDisponible = cantidadInicial;
+            this.stockMinimo = stockMinimo;
+        }
+
+        public void ReponerStock(int cantidad)
+        {
+            if (cantidad <= 0)
+            {
+                throw new ArgumentException(
+                    "La cantidad a reponer debe ser mayor que cero.");
+            }
+
+            cantidadDisponible += cantidad;
+        }
+
+        public bool HayStockSuficiente(int cantidad)
+        {
+            return cantidad > 0 && cantidadDisponible >= cantidad;
+        }
+
+        public void DescontarStock(int cantidad)
+        {
+            if (!HayStockSuficiente(cantidad))
+            {
+                throw new InvalidOperationException(
+                    $"No hay stock suficiente de {Producto.Nombre}.");
+            }
+
+            cantidadDisponible -= cantidad;
+        }
     }
 }
