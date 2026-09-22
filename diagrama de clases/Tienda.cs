bec5c0f30ecb0c5db68ef468d50976fa3f49e33b
Junemc <manu20000404@gmail.com>
2026-09-22 09:31:11 -0300

    Update Tienda.cs

--- diagrama de clases/Tienda.cs
+++ diagrama de clases/Tienda.cs
@@ -10,7 +10,20 @@ namespace diagrama_de_clases
     {
         public class Producto
         {
-            
+            private string nombre;
+           private string direccion;
+        }
+        {
+            public Tienda (string nombre, string dirección)
+            {
+                this.nombre=nombre;
+                this.direccion=direccionL;}
+            {
+                public void RegistrarVenta(
+            }
+        }
+        
+        
             
         }
     }
